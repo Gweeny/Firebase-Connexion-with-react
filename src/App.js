3d2d0f200afcccb9338f50687e0f385b812aedf7
Sam <samya.hussain@hotmail.fr>
2022-08-13 00:02:52 +0200

    third commit

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import Connexion from './connexion/connexion';
 import Test from './testProps/test';
 
 function App() {
-  const bonjour = <Test name = "John" age = "11" adresse = "12 rue de nawak" date = "12/04/11x" />
   return (
     <div className="App">
       <header className="App-header">
