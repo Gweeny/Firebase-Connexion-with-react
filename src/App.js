b0744d1752cda0394b5291dace7424d683c8198b
Sam <samya.hussain@hotmail.fr>
2022-08-13 00:01:22 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -3,13 +3,12 @@ import Connexion from './connexion/connexion';
 import Test from './testProps/test';
 
 function App() {
-  const bonjour = <Test name = "John" age = "11" adresse = "12 rue de nawak" date = "12/04/11" />
+  const bonjour = <Test name = "John" age = "11" adresse = "12 rue de nawak" date = "12/04/11x" />
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Test</h1>
-        {/* <Connexion />: */}
-        {bonjour}
+        <h1>Connexion firebase</h1>
+        <Connexion />
       </header>
     </div>
   );
